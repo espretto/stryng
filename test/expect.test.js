e820e385630e39743f76f69943e8cce3d10d62b2
espretto <espretto@gmail.com>
2013-12-02 02:31:01 +0100

    general purpose test

---
+++ test/expect.test.js
@@ -0,0 +1,16 @@
+
+expect = require('expect.js');
+
+// checkout sinonjs.org
+
+describe('function', function(){
+
+	it('should be callable', function (done) {
+		
+		var fn = function(){ done() };
+
+		expect(fn).not.to.throw();
+
+	});
+
+})
