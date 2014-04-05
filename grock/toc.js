98aaf6967182a465338215557253f26c49fb459b
Marius Runge <espretto@gmail.com>
2014-04-05 19:19:38 +0200

    grock added

---
+++ grock/toc.js
@@ -0,0 +1,82 @@
+window.files = [
+{
+  "path": "stryng.js.html",
+  "originalName": "stryng.js",
+  "originalPath": "stryng.js",
+  "name": "stryng.js.html",
+  "lang": "javascript",
+  "title": "baseline setup",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "baseline-setup",
+      "title": "baseline setup"
+    },
+    {
+      "level": 1,
+      "slug": "method-shortcuts",
+      "title": "method shortcuts"
+    },
+    {
+      "level": 2,
+      "slug": "static-methods",
+      "title": "static methods"
+    },
+    {
+      "level": 2,
+      "slug": "instance-methods",
+      "title": "instance methods"
+    },
+    {
+      "level": 1,
+      "slug": "the-whitespace-shim",
+      "title": "the whitespace shim"
+    },
+    {
+      "level": 1,
+      "slug": "type-safety",
+      "title": "type safety"
+    },
+    {
+      "level": 2,
+      "slug": "classes",
+      "title": "classes"
+    },
+    {
+      "level": 2,
+      "slug": "optimize-and-polyfill",
+      "title": "optimize and polyfill"
+    },
+    {
+      "level": 1,
+      "slug": "utility-functions",
+      "title": "utility functions"
+    },
+    {
+      "level": 2,
+      "slug": "tointeger",
+      "title": "toInteger"
+    },
+    {
+      "level": 2,
+      "slug": "exit",
+      "title": "exit"
+    },
+    {
+      "level": 1,
+      "slug": "main",
+      "title": "Main"
+    },
+    {
+      "level": 2,
+      "slug": "aliases",
+      "title": "aliases"
+    },
+    {
+      "level": 2,
+      "slug": "export",
+      "title": "export"
+    }
+  ]
+}
+];
